d3a2434265cac1d6a656bfacca14db2e6fcf343b
Rishabh <59335537+rbSparky@users.noreply.github.com>
2025-06-29 23:34:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 100 % static export
+  output: 'export',
+  // If you use <Image>, add this
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
