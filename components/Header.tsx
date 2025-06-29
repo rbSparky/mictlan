a3f1b24acbb3e53ac624688b08dda12c09fe1680
rb <rishb369@gmail.com>
2025-06-30 01:03:08 +0530

    next

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,25 +6,19 @@ import Button from "./Button";
 const Header = () => {
   return (
     <header className="flex justify-between items-center px-6 py-4 backdrop-blur-md sticky top-0 z-20 bg-gradient-to-r from-[#E0E7FD] to-[#FDFEFF] shadow-md">
-      <Image src={Logo} alt="Logo" className="cursor-pointer"/>
+      <Image
+        src={Logo}
+        alt="Logo"
+        className="h-8 w-auto md:h-10 lg:h-12 cursor-pointer"
+      />
       <FaBars className="block md:hidden" />
       <nav className="hidden md:block">
         <ul className="flex gap-6 items-center">
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Features</a>
-          </li>
-          <li>
-            <a href="#">Customers</a>
-          </li>
-          <li>
-            <a href="#">Updates</a>
-          </li>
-          <li>
-            <a href="#">Help</a>
-          </li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Customers</a></li>
+          <li><a href="#">Updates</a></li>
+          <li><a href="#">Help</a></li>
           <Button text="Get for free" />
         </ul>
       </nav>
