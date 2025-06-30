1d8a978ffdec17bd45fb37d050f6aadfa34f9cb6
Quainsami <141471455+Quainsami@users.noreply.github.com>
2025-06-30 13:13:33 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,10 +26,10 @@ const Hero = () => {
       <div className="md:flex items-center justify-center gap-16">
         <div className="md:w-[478px]">
           <div className="border-2 w-fit p-0.5 px-1 lg:text-lg rounded-lg border-slate-400/80">
-            LLM-resistant assessments
-          </div>
-          <div className="text-5xl md:text-7xl font-black my-7 bg-gradient-to-b from-black to-[#002499] text-transparent bg-clip-text tracking-tighter">
             Scam-proof tech hiring
+          </div>
+          <div className="text-5xl md:text-7xl font-black my-7 bg-gradient-to-b from-black to-[#002499] text-transparent bg-clip-text tracking-tighter">\
+            LLM-resistant assessments
           </div>
           <div className="text-xl lg:text-2xl tracking-tighter opacity-85">
             71 % of candidates admit to cheating. Mictlan AI reduces it to 0.
