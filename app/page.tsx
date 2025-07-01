d57783bb9ae26894e2257b127bba1e09b6abd02b
Quainsami <141471455+Quainsami@users.noreply.github.com>
2025-07-01 12:55:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,20 @@ import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import ProductCard from "@/components/ProductCard";
 import ProductShowcase from "@/components/ProductShowcase";
-import Testimonials from "@/components/Testimonials";
+// import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Hero/>
-      <BrandSlide/>
-      <ProductShowcase/>
-      <ProductCard/>
-      <Pricing/>
-      <Testimonials/>
-      <CTA/>
-      <Footer/>
+      <Hero />
+      <BrandSlide />
+      <ProductShowcase />
+      <ProductCard />
+      <Pricing />
+      {/* <Testimonials /> */}
+      <CTA />
+      <Footer />
     </div>
   );
 }
